93b6c355c3dc7b97d58c31bcf0a3395b357bddd1
Ramzi Carter Jr <Ramzi.carter@sjsu.edu>
2026-05-26 10:15:49 -0700

    add CA housing file to practice end to end ML Project

---
+++ ML File/CAhousing.py
@@ -0,0 +1,18 @@
+#Ramzi Carter Jr.
+#05/26/2026
+
+# End to end ML project
+#This data is a modified version of a dataset from username:Ageron on github
+#He modified the dataset which another user originally got from the now closed
+#StatLib repository. This dataset appeared in a paper from 1997 titled: Sparse Spatial Autoregressions
+#It was published in a statistics and probability letters journal using CA 1990 census data
+
+#----------------------------------
+
+#Clarifications
+#Data set contains one row per block; a block is the smallest geographical group that the census publishes sample data (300-6000 people)
+#207 values were removed at random from the total_bedrooms column and an ocean_proximity category was added to discuss categorical data
+#block group was confusing for some in the terminology so they were renamed to districts
+
+
+
