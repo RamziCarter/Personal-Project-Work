8818fcc569b2964d900e038f6da49350bb359b78
Ramzi Carter Jr <Ramzi.carter@sjsu.edu>
2026-05-31 20:14:04 -0700

    start CA housing code, fetch, download and extract file and store dataframe in housing variable

--- ML File/CAhousing.py
+++ ML File/CAhousing.py
@@ -14,5 +14,27 @@
 #207 values were removed at random from the total_bedrooms column and an ocean_proximity category was added to discuss categorical data
 #block group was confusing for some in the terminology so they were renamed to districts
 
+#MAE(Mean absolute Error) L1 norm, RMSE(Root Mean Square Error) L2 norm
+
+#Script to download the data
+from pathlib import Path
+import pandas as pd
+import tarfile
+import urllib.request
+
+def load_housing_data():
+    tarball_Path = Path("datasets/housing.tgz")
+    if not tarball_Path.is_file():
+        Path("datasets").mkdir(parents=True, exist_ok=True)
+        url = "https://github.com/ageron/data/raw/main/housing.tgz"
+        urllib.request.urlretrieve(url, tarball_Path)
+        with tarfile.open(tarball_Path) as housing_tarball:
+            housing_tarball.extractall(path="datasets")
+    return pd.read_csv("datasets/housing/housing.csv")
+
+housing = load_housing_data()
+
+
+
 
 
